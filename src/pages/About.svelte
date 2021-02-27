daa1ce821296e3469abc28853bdd0343ac85b9be
weidelix <dev.mesina@gmail.com>
2021-02-27 20:15:15 +0800

    Cleaned up files

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
-	import { url, metatags } from '@roxi/routify';
-	metatags.title = 'About';
+
 </script>
 
-<!-- routify:options index=1 -->
 <div class="grid p-6">
 	<h1 class="text-6xl font-black">About</h1>
 	<p class="text-xl font-base text-gray-500 py-2">
